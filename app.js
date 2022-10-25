56ba902b2e80728d2bdaf3c1e4cc5ed27825c520
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-10-25 19:52:41 +0300

    Finished header and subtitle responsive

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-"use strict";
+import { headerBtnsText } from "./utils/constants.js";
+
+("use strict");
 const circle = document.querySelectorAll(".circle");
 const timerValue = document.querySelectorAll(".timer_value");
-
 const headerImageContainer = document.querySelector(".header-img_container");
+const headerBtns = document.querySelector(".header-btns");
 
-function timer() {
+// Timer
+let progressBarSec, progressBarMin, progressBarHour, progressBarDay;
+
+function displayTimer() {
   const countDate = new Date("November 24, 2022 00:00:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
@@ -19,33 +24,44 @@ function timer() {
   const textMin = Math.floor((gap % hour) / min);
   const textSec = Math.floor((gap % min) / sec);
 
-  let progressSec = textSec;
-  let progressMin = textMin;
-  let progressHour = textHour;
-  let progressDay = textDay;
-
-  timerValue[0].textContent = progressDay;
-  timerValue[1].textContent = progressHour;
-  timerValue[2].textContent = progressMin;
-  timerValue[3].textContent = progressSec;
+  timerValue[0].textContent = textDay;
+  timerValue[1].textContent = textHour;
+  timerValue[2].textContent = textMin;
+  timerValue[3].textContent = textSec;
 
   // prettier-ignore
-  circle[0].style.background = `conic-gradient(var(--circle-one) ${progressDay * 12}deg,var(--circle-default) 0deg)`;
+  circle[0].style.background = `conic-gradient(var(--circle-one) ${textDay * 12}deg,var(--circle-default) 0deg)`;
   // prettier-ignore
-  circle[1].style.background = `conic-gradient(var(--circle-two) ${progressHour * 15}deg,var(--circle-default) 0deg)`;
+  circle[1].style.background = `conic-gradient(var(--circle-two) ${textHour * 15}deg,var(--circle-default) 0deg)`;
   // prettier-ignore
-  circle[2].style.background = `conic-gradient(var(--circle-three) ${progressMin * 6}deg,var(--circle-default) 0deg)`;
+  circle[2].style.background = `conic-gradient(var(--circle-three) ${textMin * 6}deg,var(--circle-default) 0deg)`;
   // prettier-ignore
-  circle[3].style.background = `conic-gradient(var(--circle-four) ${progressSec * 6}deg,var(--circle-default) 0deg)`;
+  circle[3].style.background = `conic-gradient(var(--circle-four) ${textSec * 6}deg,var(--circle-default) 0deg)`;
 }
 
-setInterval(() => {
-  timer();
-}, 1000);
+function timerHandler() {
+  // prettier-ignore
+  const circleColors = ["--circle-one","--circle-two","--circle-three","--circle-four"];
+  // prettier-ignore
+  if (!progressBarDay && !progressBarHour && !progressBarMin && !progressBarSec) {
+    setInterval(() => {
+      displayTimer();
+    }, 1000);
+  } else {
+    clearInterval();
+    timerValue.forEach((item) => {
+      item.textContent = 0;
+    });
+    circle.forEach((item, index) => {
+      item.style.background = `conic-gradient(var(${circleColors[index]}) 3deg,var(--circle-default) 0deg)`;
+    });
+  }
+}
+timerHandler();
 
 function showHeaderImage(view) {
   const desktopHeaderImage = "./assets/headerImg.png";
-  const mobileHeaderImage = "./assets/headerImageMobile.png";
+  const mobileHeaderImage = "./assets/headerImgMobile.png";
 
   if (view.matches) {
     headerImageContainer.innerHTML = `<img class="header_img width_full" src="${mobileHeaderImage}" alt="header image" />`;
@@ -56,3 +72,20 @@ function showHeaderImage(view) {
 const view = window.matchMedia("(max-width: 450px)");
 showHeaderImage(view);
 view.addListener(showHeaderImage);
+
+headerBtnsText.map((item) => {
+  const headerBtnsContainer = document.createElement("div");
+  headerBtnsContainer.classList.add("header-btns_container");
+  headerBtnsContainer.innerHTML = `
+    <a href="#" class="btn-primary header-buttons_button">${item}</a>
+  `;
+  headerBtns.appendChild(headerBtnsContainer);
+});
+
+function headerBtnsCustomStyle() {
+  const headerBtnsButton = document.querySelectorAll(".header-buttons_button");
+
+  headerBtnsButton[0].style.background = "var(--bg-secondary-clr)";
+  headerBtnsButton[4].style.outline = "3px solid var(--bg-secondary-clr)";
+}
+headerBtnsCustomStyle();
---
+++ utils/constants.js
@@ -0,0 +1,10 @@
+export const headerBtnsText = [
+  "Toate produsele",
+  "Credit imobiliar - ipotecar",
+  "Credit de nevoi personale (4)",
+  "Card STAR (3)",
+  "Festival Stuff",
+  "Pachet Credit nevoi dobanda fixa + Card STAR (3)",
+  "Pachet Credit nevoi dobanda variabila + Card STAR (3)",
+  "Leasing auto (2)",
+];
